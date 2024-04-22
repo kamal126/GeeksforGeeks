2ffe02ce421c6d9c6ce404ebf450585cc7bc20cf
Kamal Chandra <54989946+kamal126@users.noreply.github.com>
2024-04-22 14:38:27 +0530

    Row with minimum number of 1's
    22 April 2024

--- minway.py
+++ minway.py
@@ -42,3 +42,40 @@ Constraints:
 
 0 <= a[i][j] <= 1
 """
+
+#User function Template for python3
+
+class Solution:
+    def minRow(self,n,m,a):
+        min_row_index = 0
+        min_count = m
+        
+        for i in range(n):
+            count = 0
+            for j in range(m):
+                if a[i][j] == 1:
+                    count += 1
+            if count < min_count:
+                min_row_index = i
+                min_count = count
+    
+        return min_row_index + 1
+    
+ 
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+        
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N,M=map(int,input().strip().split(" "))
+        A=[]
+        for i in range(N):
+            B=list(map(int,input().strip().split(" ")))
+            A.append(B)
+        ob=Solution()
+        print(ob.minRow(N,M,A))
+# } Driver Code Ends
